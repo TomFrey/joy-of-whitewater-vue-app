42ceca5bdbd56092ec52c97980a3a3fac37e8273
Thomas Frey <joe@mitlinxlernen.ch>
2022-01-18 16:58:51 +0100

    Filter Component erstellt. Filter 'Kursstufe' implementiert.

---
+++ src/components/TableFilter.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="course-filter">
+        <div class="course-filter__item">
+            <label for="from">Datum von</label>
+            <input id="from" type="text" v-model="fromDate" placeholder="01.01.2020">
+        </div>
+        <div class="course-filter__item">
+            <label for="to">Datum bis</label>
+            <input id="to" type="text" v-model="toDate" placeholder="31.12.2020">
+        </div>
+    </div>
+    <div class="course-filter">
+        <div class="course-filter__item">
+            <label for="level">Kursstufe</label>
+            <select id="level" v-model="wwLevel">
+                <option></option>
+                <option>B</option>
+                <option>F</option>
+                <option>K</option>
+                <option>alle</option>
+            </select>
+        </div>
+        <div class="course-filter__item">
+            <label for="sport">Sportart</label>
+            <input id="sport" type="text" v-model="sportKind" placeholder="Kajak, Kanadier ...">
+        </div>
+        <div class="course-filter__item">
+            <label for="typ">Kurstyp</label>
+            <input id="typ" type="text" v-model="courseType" placeholder="Kanukurs, Paddelreise ...">
+        </div>
+        <div class="course-filter__item">
+            <label for="name">Kursname</label>
+            <input id="name" type="text" v-model="courseName" placeholder="Kajak Aufbaukurs">
+        </div>
+        <p>kursStufe: {{ wwLevel }}</p>
+    </div>
+    <div>
+        <button @click='changeFilter()'>Filter</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TableFilter',
+  emits: ['filterChanged'],
+
+  data () {
+    return {
+      wwLevel: '',
+      sportKind: '',
+      courseType: '',
+      courseName: '',
+      fromDate: '',
+      toDate: ''
+    }
+  },
+  methods: {
+    changeFilter () {
+      console.log('changeFilter...')
+      this.$emit('filterChanged', this.wwLevel)
+    }
+  }
+//   computed: {
+//     wwLevel: {
+//       get () {
+//         return this.wwLevel
+//       },
+//       set (value) {
+//         this.$emit('update:wwLevel', value)
+//       }
+//     }
+//   }
+}
+</script>
+
+<style lang="scss" scoped>
+    .course-filter{
+        display: flex;
+        background-color: aqua;
+        margin-bottom: 16px;
+
+        .course-filter__item{
+            display: flex;
+            flex-flow: column;
+            align-items: flex-start;
+            margin-right: 16px;
+        }
+    }
+</style>
--- src/views/Kurse.vue
+++ src/views/Kurse.vue
@@ -2,8 +2,11 @@
   <div class="kurse">
     <h1>This is Kurse page</h1>
 
+    <table-filter @filterChanged="filterWWLevel($event)">
+    </table-filter>
+
     <table>
-      <tr v-for="course in courses" :key="course.id">
+      <tr v-for="course in coursesToShow" :key="course.id">
         <td>{{ course.id }}</td>
         <td>{{ course.name }}</td>
         <td>{{ course.sportArt }}</td>
@@ -16,26 +19,46 @@
 </template>
 
 <script>
+import TableFilter from '@/components/TableFilter'
 import CourseService from '@/services/CourseService.js'
 
 export default {
+  components: { TableFilter },
 
   data () {
     return {
-      courses: []
+      courses: [],
+      coursesToShow: []
     }
   },
 
   // LifeCicleHook created()
   created () {
+    console.log('test -> ' + this.wwLevel)
     CourseService.getCourses()
       .then(response => {
         console.log(response.data)
         this.courses = response.data
+        this.coursesToShow = response.data
       })
       .catch(error => {
         console.log('Da war ein Fehler: ' + error.response)
       })
+  },
+
+  methods: {
+    filterWWLevel (wwLevel) {
+      console.log('test2 -> ' + wwLevel)
+
+      if (wwLevel !== '') {
+        const filteredCourses = this.courses.filter((course) => {
+          return course.kursStufe === wwLevel
+        })
+        this.coursesToShow = filteredCourses
+      } else {
+        this.coursesToShow = this.courses
+      }
+    }
   }
 
 }
